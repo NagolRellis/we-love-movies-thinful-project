b812e5f28c25c2640173ad59c4be4c33661b2801
Logan Meritt <101428688+NagolRellis@users.noreply.github.com>
2023-01-29 11:48:23 -0800

    Update knexfile.js

--- knexfile.js
+++ knexfile.js
@@ -2,20 +2,9 @@ const path = require("path");
 
 require("dotenv").config();
 
-// const {
-//   URL = "postgresql://postgres@localhost/postgres",
-// } = process.env;
-
-const { 
-  NODE_ENV = 'development',
-  DEVELOPMENT_DATABASE_URL,
-  PRODUCTION_DATABASE_URL
- } = process.env;
-
-const URL = 
-    NODE_ENV === "production"
-       ? PRODUCTION_DATABASE_URL
-       : DEVELOPMENT_DATABASE_URL;
+const {
+   URL = "postgres://taucsoyt:DxHnrVwF1o8fnoNPAs0jLnhHMrtBWcz-@kashin.db.elephantsql.com/taucsoyt",
+} = process.env;
 
 
 module.exports = {
